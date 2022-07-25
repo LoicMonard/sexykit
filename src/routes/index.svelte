e62ef5932062b6212c3437bb424929467ca554e6
LoicMonard <monard@intechinfo.fr>
2022-07-25 23:17:17 +0200

    :bento: Add PulpDisplay fonts to the project

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,10 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Button primary label="Salut la mif" />
+
+
+<script>
+// Import button
+import Button from "$components/Button.svelte";
+</script>
+
