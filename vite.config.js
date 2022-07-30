37db6c16ec7212fa9af9b1e7ec88d54f8b7192d8
Loïc Monard <monard@intechinfo.fr>
2022-07-30 16:12:51 +0200

    :heavy_plus_sign: Add fontAwesome dependencies through CDNs

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
-	// allow locale files to be loaded
-	server: {
-		fs: {
-			allow: ['..']
-		}
-	}
-
 };
 
 export default config;
