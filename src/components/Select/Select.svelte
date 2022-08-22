2dbe5d8a8c493c24b4e3bfd61dbc788037b3599d
Loïc Monard <monard@intechinfo.fr>
2022-08-22 23:01:56 +0200

    :sparkles: Select component is now working at it first version, with autocomplete filtering

--- src/components/Select/Select.svelte
+++ src/components/Select/Select.svelte
@@ -9,22 +9,42 @@
 	export let size = 'medium';
 	export let icon = null;
 	export let options = [];
+	export let valueKey = null;
 
-	let open = true;
+	$: filteredOptions = options.filter((o) => o.label.includes(displayedValue));
+
+	$: displayedValue = value ? (valueKey ? value[valueKey] : value.label || '') : '';
+
+	let open = false;
+
+	let tmpValue;
+	let tmpPlaceholder;
 
 	const toggleSelect = (...args) => {
-		const [, bool, ,] = args;
+		if (value) {
+			tmpValue = value;
+			placeholder = tmpValue.label;
+		}
+		tmpPlaceholder = placeholder;
+		value = '';
+		const [, bool, from] = args;
+		if (!value && from === 'blur') {
+			value = tmpValue;
+			placeholder = tmpPlaceholder;
+		}
 		open = bool;
 	};
-</script>
 
-{open}
+	const selectOption = (option) => {
+		value = valueKey ? option[valueKey] : option;
+	};
+</script>
 
 <div class="select">
 	<div class="select__wrapper">
 		<Textfield
 			type="select"
-			bind:value
+			bind:value={displayedValue}
 			{placeholder}
 			{icon}
 			{size}
@@ -36,12 +56,16 @@
 		</Textfield>
 		{#if open === true}
 			<div class="select__options" transition:fly={{ y: -10, duration: 300 }}>
-				{#each options as option}
-					<div class="select__option">
-						<input type="radio" name="select" value={option.value} />
+				{#each filteredOptions as option}
+					<div class="select__option" on:click={selectOption(option)}>
 						<label>{option.label}</label>
 					</div>
 				{/each}
+				{#if filteredOptions.length === 0}
+					<div class="select__option select__option__empty">
+						<label>Aucun résultat</label>
+					</div>
+				{/if}
 			</div>
 		{/if}
 	</div>
