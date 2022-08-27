1c3da6a5549f2c713b9c7d3709b7652db3345bcc
Loïc Monard <monard@intechinfo.fr>
2022-08-27 23:49:14 +0200

    :sparkles: Create Container component

---
+++ src/components/Container/Container.svelte
@@ -0,0 +1,24 @@
+<script>
+	import './container.scss';
+
+	export let direction = 'row';
+	export let justify = 'start';
+	export let align = 'start';
+	export let gap = '0px';
+	export let fullWidth = false;
+	export let padding = '0px';
+</script>
+
+<div
+	class="container"
+	style="
+		flex-direction: {direction};
+	 	justify-content: {justify}; 
+		align-items: {align}; 
+		gap: {gap}; 
+		padding: {padding}; 
+		width: {fullWidth ? '100%' : 'fit-content'};
+	"
+>
+	<slot />
+</div>
