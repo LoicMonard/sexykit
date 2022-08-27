dc9cbddac993c7667139a0283459160dae27faf1
Loïc Monard <monard@intechinfo.fr>
2022-08-27 23:48:51 +0200

    :sparkles: Create Card component and stories

---
+++ src/components/Card/Card.stories.js
@@ -0,0 +1,23 @@
+import Card from './Card.svelte';
+
+export default {
+	title: 'Components/Card',
+	component: Card,
+	parameters: {
+		// More on Story layout: https://storybook.js.org/docs/svelte/configure/story-layout
+		layout: 'fullscreen'
+	},
+	argTypes: {
+		fullWidth: { control: 'boolean' }
+	}
+};
+
+const Template = (args) => ({
+	Component: Card,
+	props: args,
+});
+
+export const Default = Template.bind({});
+Default.args = {
+	fullWidth: true,
+};
---
+++ src/components/Card/Card.svelte
@@ -0,0 +1,17 @@
+<script>
+	import './card.scss';
+
+	export let fullWidth = false;
+</script>
+
+<div class="card" style={fullWidth ? 'width: 100%;' : 'width: fit-content'}>
+	<!-- Title slot -->
+	{#if $$slots.header}
+		<div class="card--header">
+			<slot name="header" />
+		</div>
+	{/if}
+	<div class="card--body">
+		<slot />
+	</div>
+</div>
