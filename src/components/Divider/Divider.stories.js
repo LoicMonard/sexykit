0236328497d3727b20d3c3dc963a2646e4b5e813
Loïc Monard <monard@intechinfo.fr>
2022-08-28 19:35:59 +0200

    :sparkles: Create Divider component stories

---
+++ src/components/Divider/Divider.stories.js
@@ -0,0 +1,18 @@
+import Divider from './Divider.svelte';
+
+export default {
+	title: 'Components/Divider',
+	component: Divider,
+	parameters: {
+		// More on Story layout: https://storybook.js.org/docs/svelte/configure/story-layout
+		layout: 'fullscreen'
+	}
+};
+
+const Template = (args) => ({
+	Component: Divider,
+	props: args
+});
+
+export const Default = Template.bind({});
+Default.args = {};
