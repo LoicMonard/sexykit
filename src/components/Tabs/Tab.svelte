5b4c66462c2580121e04942959203b9c85db9f93
Loïc Monard <monard@intechinfo.fr>
2022-09-06 00:18:32 +0200

    :memo: Add documentation on both Tabs and Tab components

--- src/components/Tabs/Tab.svelte
+++ src/components/Tabs/Tab.svelte
@@ -4,22 +4,37 @@
 
 	import './tab.scss';
 
+	/**
+	 * The tab label
+	 */
 	export let name;
 
-	const { registerTab } = getContext('registerTab');
-	const { activeTab } = getContext('activeTab');
-
+	/**
+	 * A reactive value that indicates if the tab is active depending on
+	 * the instance props
+	 */
 	$: active = $activeTab === $$props;
 
+	/**
+	 * A function to access the parent registerTab method
+	 */
+	const { registerTab } = getContext('registerTab');
+	/**
+	 * A function to access the parent selectTab method
+	 */
+	const { activeTab } = getContext('activeTab');
+
+	/**
+	 * A function that register the tab to the parent component when
+	 * the component is mounted
+	 */
 	onMount(() => {
 		registerTab($$props);
 	});
 </script>
 
 {#if active}
-	<div
-		class={['tab', active ? 'tab--active' : ''].join(' ')}
-	>
+	<div class={['tab', active ? 'tab--active' : ''].join(' ')}>
 		<slot />
 	</div>
 {/if}
--- src/components/Tabs/Tabs.svelte
+++ src/components/Tabs/Tabs.svelte
@@ -1,15 +1,62 @@
 <script>
+	import './tabs.scss';
 	import { setContext } from 'svelte';
 	import { tick } from 'svelte';
 	import { writable } from 'svelte/store';
 	import Card from '../Card/Card.svelte';
 
-	import './tabs.scss';
-
+	/**
+	 * The list of the displayed tabs
+	*/
 	let tabs = [];
-
+	/**
+	 * The active tab, which is passed to the tab components.
+	 * Init as a store variable to enable the reactivity in the child
+	 * components
+	 */
 	let activeTab = writable(null);
+	/**
+	 * The computed slider width, based on the active tab
+	 */
+	let sliderWidth;
+	/**
+	 * The computed slider position, based on the active tab
+	*/
+	let sliderOffset;
 
+	/**
+	 * A function that is called when a tab is clicked, or during
+	 * tab initialization. It sets the active tab and computes the
+	 * slider position and width
+	 * 
+	 * @param tab
+	 */
+	const setActiveTab = async (tab) => {
+		$activeTab = tab;
+		await tick();
+		computeSliderWidth();
+		computeSliderOffset();
+	};
+	/**
+	 * A function that computes the slider width based on the active tab
+	 */
+	const computeSliderWidth = () => {
+		const tabWidth = document.querySelector('.tabs__header .tabs__nav--active').offsetWidth;
+		sliderWidth = tabWidth;
+	};
+	/**
+	 * A function that computes the slider offset based on the active tab
+	 */
+	const computeSliderOffset = () => {
+		const tabLeft = document.querySelector('.tabs__header .tabs__nav--active').offsetLeft;
+		sliderOffset = tabLeft;
+	};
+	
+	/**
+	 * A function that is called when a tab is initialized.
+	 * It adds the tab to the tabs list and set the first tab as active
+	 * if there is no active tab
+	*/
 	setContext('registerTab', {
 		registerTab: (tab) => {
 			tabs = [...tabs, tab];
@@ -18,29 +65,14 @@
 			}
 		}
 	});
-
-	const setActiveTab = async (tab) => {
-		$activeTab = tab;
-		await tick();
-		computeSliderWidth();
-		computeSliderOffset();
-	};
-
+	/**
+	 * A function that passes the setActiveTab function to the child
+	*/
 	setContext('setActiveTab', setActiveTab);
-
+	/**
+	 * A function that passes the activeTab store to the child
+	*/
 	setContext('activeTab', { activeTab });
-
-	let sliderWidth;
-	let sliderOffset;
-
-	const computeSliderWidth = () => {
-		const tabWidth = document.querySelector('.tabs__header .tabs__nav--active').offsetWidth;
-		sliderWidth = tabWidth;
-	};
-	const computeSliderOffset = () => {
-		const tabLeft = document.querySelector('.tabs__header .tabs__nav--active').offsetLeft;
-		sliderOffset = tabLeft;
-	};
 </script>
 
 <div class="tabs">
