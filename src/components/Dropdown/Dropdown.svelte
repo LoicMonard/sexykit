524d74b61c1b8bce14e5cc0f8d6d7fbec3f5362f
Loïc Monard <monard@intechinfo.fr>
2022-09-06 21:58:20 +0200

    :sparkles: Add Dropdown component

---
+++ src/components/Dropdown/Dropdown.svelte
@@ -0,0 +1,50 @@
+<script>
+	import './dropdown.scss';
+	import { createEventDispatcher, tick } from 'svelte';
+	import { clickOutside } from '$utils/functions';
+	import ItemList from '../ItemList/ItemList.svelte';
+
+	/**
+	 * The items of the list
+	 */
+	export let items = [];
+
+	/**
+	 * If the dropdown is toggled
+	 */
+	let open = false;
+
+	/**
+	 * An event dispatcher
+	 */
+	const dispatch = createEventDispatcher();
+	/**
+	 * A function that is triggered when an option gets clicked,
+	 * and dispatches the event with the selected item
+	 *
+	 * @param e contains the clicked option
+	 */
+	const selectOption = async (e) => {
+		const option = e.detail;
+		if (!option.disabled) {
+			await tick();
+			dispatch('change', option);
+			open = false;
+		}
+	};
+	/**
+	 * A function that closes the dropdown items when clicked outside
+	 */
+	const handleClickOutside = () => {
+		open = false;
+	};
+</script>
+
+<div class="dropdown" use:clickOutside on:click_outside={handleClickOutside}>
+	<div class="dropdown__trigger" on:click={() => (open = !open)}>
+		<slot />
+	</div>
+	{#if open}
+		<ItemList {items} on:mousedown={selectOption} />
+	{/if}
+</div>
