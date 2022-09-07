f5cb2b5a8eb57463027fccb5cf46249a73b69b8a
Loïc Monard <monard@intechinfo.fr>
2022-09-08 00:00:19 +0200

    :sparkles: Add packaging configuration

---
+++ src/lib/index.js
@@ -0,0 +1,11 @@
+import Button from './_components/Button/Button.svelte';
+import Card from './_components/Card/Card.svelte';
+import Container from './_components/Container/Container.svelte';
+import Divider from './_components/Divider/Divider.svelte';
+import Dropdown from './_components/Dropdown/Dropdown.svelte';
+import Tabs from './_components/Tabs/Tabs.svelte';
+import Tab from './_components/Tabs/Tab.svelte';
+import Textfield from './_components/Textfield/Textfield.svelte';
+import Select from './_components/Select/Select.svelte';
+
+export { Button, Card, Container, Divider, Dropdown, Tabs, Tab, Textfield, Select };
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
+		prerender: { default: true },
 		alias: {
 			$components: 'src/components',
 			$styles: 'src/styles',
