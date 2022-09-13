99edebb19a6d7c1aad528f96adb4a9877a75cded
Loïc Monard <monard@intechinfo.fr>
2022-09-13 22:49:05 +0200

    :sparkles: Create Text component

---
+++ src/lib/_components/Text/Text.svelte
@@ -0,0 +1,18 @@
+<script>
+	import './text.scss';
+
+	export let size = 'regular';
+
+	export let bold;
+
+	export let italic;
+
+	export let color;
+</script>
+
+<span
+	class={`text text--${size}${bold ? ' text--bold' : ''}${italic ? ' text--italic' : ''}`}
+	style="color: {color}"
+>
+	<slot />
+</span>
