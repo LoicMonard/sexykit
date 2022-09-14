2f08793994fc21ab78a7f8a40b441cd951151aaa
Loïc Monard <monard@intechinfo.fr>
2022-09-14 18:06:05 +0200

    :sparkles: Add shadow property on Card component

--- src/lib/_components/Card/Card.svelte
+++ src/lib/_components/Card/Card.svelte
@@ -5,9 +5,13 @@
 	 * If the card should take the full width of its container
 	 */
 	export let fullWidth = false;
+	/**
+	 * If the card should take the full width of its container
+	 */
+	 export let shadow = 'hover';
 </script>
 
-<div class="card" style={fullWidth ? 'width: 100%;' : 'width: fit-content'}>
+<div class={`card card-shadow--${shadow}`} style={fullWidth ? 'width: 100%;' : 'width: fit-content'}>
 	{#if $$slots.header}
 		<div class="card--header">
 			<slot name="header" />
