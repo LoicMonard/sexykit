9c01b6b02931b47128072854486296fc703b2c8e
Loïc Monard <monard@intechinfo.fr>
2022-09-15 13:02:39 +0200

    :green_heart: Update base url for CI static loading

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: 'index.html'
+			fallback: 'index.html',
+			assets: 'build',
 		}),
+		paths: {
+			base: dev ? '' : '/sexykit',
+		},
+		appDir: dev ? 'src' : 'internal',
     prerender: { entries: [] },
 		alias: {
 			$components: 'src/components',
