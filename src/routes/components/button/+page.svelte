1b3e698c83c30f6186945cf6648584234b381220
Loïc Monard <monard@intechinfo.fr>
2022-10-17 16:15:27 +0200

    :coffin: Remove codeBlock related code

--- src/routes/components/button/+page.svelte
+++ src/routes/components/button/+page.svelte
@@ -3,29 +3,11 @@
 
 	import Button from '$lib/_components/Button/Button.svelte';
 	import Card from '$lib/_components/Card/Card.svelte';
-	import Collapse from '$lib/_components/Collapse/Collapse.svelte';
-	import CollapseItem from '$lib/_components/Collapse/CollapseItem.svelte';
 	import Container from '$lib/_components/Container/Container.svelte';
 	import CodeBlock from '$components/CodeBlock/CodeBlock.svelte';
 	import Text from '$lib/_components/Text/Text.svelte';
-	import xml from 'svelte-highlight/languages/xml';
-	import { onMount } from 'svelte';
-
-	let highlightComponent;
-	let highlightStyles;
-
-	onMount(async () => {
-		xml.name = 'svelte';
-		highlightComponent = (await import('svelte-highlight')).Highlight;
-		highlightStyles = (await import('svelte-highlight/styles/atom-one-dark')).default;
-	});
 </script>
 
-<svelte:head>
-	{#if highlightStyles}
-		{@html highlightStyles}
-	{/if}
-</svelte:head>
 
 <Container direction="column" gap="20px" fullWidth>
 	<Text>Comonly used button</Text>
