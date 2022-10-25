31574690aec400c0d2d445f77359a799a5a166d0
Loïc Monard <monard@intechinfo.fr>
2022-10-25 17:35:21 +0200

    :sparkles: Add style property on Header component

--- src/lib/_components/Header/Header.svelte
+++ src/lib/_components/Header/Header.svelte
@@ -2,10 +2,17 @@
 	import './header.scss';
 	import Container from '../Container/Container.svelte';
 
+	/**
+	 * If the header is fixed or not
+	 */
 	export let fixed = false;
+	/**
+	 * The style of the header
+	 */
+	export let style = '';
 </script>
 
-<header class={`header ${fixed ? 'header--fixed' : ''}`}>
+<header class={`header ${fixed ? 'header--fixed' : ''}`} {style}>
 	<Container direction="row" fullWidth fullHeight align="center">
 		<slot />
 	</Container>
