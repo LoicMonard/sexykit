62c1fcfe9866d1de0e1dbe1b8f6bb1fa32637f90
Loïc Monard <monard@intechinfo.fr>
2022-10-26 19:10:56 +0200

    :package: Update exported components list for packaging
    Signed-off-by: Loïc Monard <monard@intechinfo.fr>

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,11 +1,33 @@
 import Button from './_components/Button/Button.svelte';
 import Card from './_components/Card/Card.svelte';
+import Collapse from './_components/Collapse/Collapse.svelte';
+import CollapseItem from './_components/Collapse/CollapseItem.svelte';
 import Container from './_components/Container/Container.svelte';
 import Divider from './_components/Divider/Divider.svelte';
 import Dropdown from './_components/Dropdown/Dropdown.svelte';
+import Header from './_components/Header/Header.svelte';
+import Modal from './_components/Modal/Modal.svelte';
+import Select from './_components/Select/Select.svelte';
+import Sidenav from './_components/Sidenav/Sidenav.svelte';
 import Tabs from './_components/Tabs/Tabs.svelte';
 import Tab from './_components/Tabs/Tab.svelte';
+import Text from './_components/Text/Text.svelte';
 import Textfield from './_components/Textfield/Textfield.svelte';
-import Select from './_components/Select/Select.svelte';
 
-export { Button, Card, Container, Divider, Dropdown, Tabs, Tab, Textfield, Select };
+export {
+	Button,
+	Card,
+	Collapse,
+  CollapseItem,
+	Container,
+	Divider,
+	Dropdown,
+	Header,
+	Modal,
+	Select,
+  Sidenav,
+	Tabs,
+	Tab,
+  Text,
+	Textfield
+};
